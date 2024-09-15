344b96878c4c536b652c0dc3b599d37e55c9aa10
Vanessa <schindler.vanessa@outlook.de>
2024-09-15 13:21:24 +0200

    inital commit

---
+++ src/components/CounterClassAPI.vue
@@ -0,0 +1,18 @@
+
+<template>
+
+</template>
+<script lang="ts">
+import { Vue, Component } from 'vue-facing-decorator'
+
+@Component({})
+export default class CounterClassAPI extends Vue {
+
+}
+</script>
+
+
+
+<style scoped>
+
+</style>
